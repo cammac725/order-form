236f14ddaa5f684085a2d8e3b75af139ba539d1c
cammac <camm@rocktechcomputer.com>
2020-08-09 11:23:22 -0700

    initial setup

---
+++ src/OrderApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class OrderApp extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default OrderApp;
